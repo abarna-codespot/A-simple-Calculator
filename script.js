8b5b05aa691b7a265267121f3e27600ff49ecb7f
msaitejareddy <55725091+msaitejareddy@users.noreply.github.com>
2021-08-10 18:56:44 -0500

    Added comments in js file

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-/*java script functions added */
+/*java script functions added
+we can create and name git remote branch from intellij itself
+
+ while doing git commit & push change branch name in remote*/
 
 function getHistory(){
 	return document.getElementById("history-value").innerText;
