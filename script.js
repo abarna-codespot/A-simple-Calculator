c0f0b2885c5064d7c5ba3c87b25316e774676e65
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-11-30 17:05:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,30 +19,30 @@ function getFormattedNumber(num){
 	if(num=="-"){
 		return "";
 	}
-	var n = Number(num);
-	var value = n.toLocaleString("en");
+	let n = Number(num);
+	let value = n.toLocaleString("en");
 	return value;
 }
 function reverseNumberFormat(num){
 	return Number(num.replace(/,/g,''));
 }
-var operator = document.getElementsByClassName("operator");
-for(var i =0;i<operator.length;i++){
+let operator = document.getElementsByClassName("operator");
+for(let i =0;i<operator.length;i++){
 	operator[i].addEventListener('click',function(){
 		if(this.id=="clear"){
 			printHistory("");
 			printOutput("");
 		}
 		else if(this.id=="backspace"){
-			var output=reverseNumberFormat(getOutput()).toString();
+			let output=reverseNumberFormat(getOutput()).toString();
 			if(output){//if output has a value
 				output= output.substr(0,output.length-1);
 				printOutput(output);
 			}
 		}
 		else{
-			var output=getOutput();
-			var history=getHistory();
+			let output=getOutput();
+			let history=getHistory();
 			if(output==""&&history!=""){
 				if(isNaN(history[history.length-1])){
 					history= history.substr(0,history.length-1);
@@ -52,7 +52,7 @@ for(var i =0;i<operator.length;i++){
 				output= output==""?output:reverseNumberFormat(output);
 				history=history+output;
 				if(this.id=="="){
-					var result=eval(history);
+					let result=eval(history);
 					printOutput(result);
 					printHistory("");
 				}
@@ -66,10 +66,10 @@ for(var i =0;i<operator.length;i++){
 		
 	});
 }
-var number = document.getElementsByClassName("number");
-for(var i =0;i<number.length;i++){
+let number = document.getElementsByClassName("number");
+for(let i =0;i<number.length;i++){
 	number[i].addEventListener('click',function(){
-		var output=reverseNumberFormat(getOutput());
+		let output=reverseNumberFormat(getOutput());
 		if(output!=NaN){ //if output is a number
 			output=output+this.id;
 			printOutput(output);
